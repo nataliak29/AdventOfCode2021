64e44a489532b4c38c5857eacdd7d8562993f715
Natalia Kukushkina <natkk@amazon.com>
2021-12-15 10:23:10 -0400

    day 8 part 2 solution

--- src/main/java/Day8.java
+++ src/main/java/Day8.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day8 extends Day {
 
@@ -12,7 +13,7 @@ public class Day8 extends Day {
 
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Part1: " + new Day8().partOneAnswer(RESOURCE));
-        System.out.println("Part2: "+ new Day8().partTwoAnswer(RESOURCE));
+        System.out.println("Part2: " + new Day8().partTwoAnswer(RESOURCE));
     }
 
     @Override
@@ -26,64 +27,98 @@ public class Day8 extends Day {
 
     @Override
     public String partTwoAnswer(String resource) {
-        ArrayList<String> inputArray = getResourceAsStringArray(resource);
-        //int[] integerList = getIntegersList(inputArray);
-        int answer = -1;
+        int totalSum = 0;
+        ArrayList<String> recordsArray = getResourceAsStringArray(resource);
+        for (String l: recordsArray){
+            ArrayList<String> outputsArray = getAllOutputs(l);
+            ArrayList<String> inputsArray = getAllInputs(l);
+            HashMap<Integer, String> decodeDigitsMap = decodeDigits(inputsArray);
+            Integer decodeOutputDigits = decodeOutputDigits(outputsArray, decodeDigitsMap);
+            totalSum += decodeOutputDigits;
+        }
+
+        int answer = totalSum;
         return String.valueOf(answer);
     }
 
-    public String[] getAllSegmentsFromLine(String inputArray){
-        return  inputArray.split(" | ");
+    private String[] getAllSegmentsFromLine(String inputArray) {
+        return inputArray.split(" | ");
     }
 
-    public String[] getSegmentsFromLine(String[] allSegmentsArray, String inputOrOutput){
+    private String[] getSegmentsFromLine(String[] allSegmentsArray, String inputOrOutput) {
         int indexOfDivider = Arrays.asList(allSegmentsArray).indexOf("|");
-        if ( inputOrOutput.equals("input")){
+        if (inputOrOutput.equals("input")) {
             String[] resultingArray = Arrays.copyOfRange(allSegmentsArray, 0, indexOfDivider);
-            return  resultingArray;
-        }
-        else {
+            return resultingArray;
+        } else {
             String[] resultingArray = Arrays.copyOfRange(allSegmentsArray, indexOfDivider + 1, allSegmentsArray.length);
-            return  resultingArray;
+            return resultingArray;
         }
     }
 
-    public ArrayList<String> getAllOutputs(ArrayList<String> rawInputArray){
+    public static String sortString(String inputString)
+    {
+        char tempArray[] = inputString.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
+    public ArrayList<String> getAllOutputs(ArrayList<String> rawInputArray) {
         ArrayList<String> outputs = new ArrayList<String>();
-        for (String line : rawInputArray){
+        for (String line : rawInputArray) {
             String[] allSegments = getAllSegmentsFromLine(line);
-            String[] lineOutputs = getSegmentsFromLine(allSegments,"output");
-            for ( String output : lineOutputs) {
-                outputs.add(output);
+            String[] lineOutputs = getSegmentsFromLine(allSegments, "output");
+            for (String output : lineOutputs) {
+                outputs.add(sortString(output));
             }
         }
-        return  outputs;
+        return outputs;
+    }
+    public ArrayList<String> getAllOutputs(String rawInput) {
+        ArrayList<String> outputs = new ArrayList<String>();
+        String[] allSegments = getAllSegmentsFromLine(rawInput);
+        String[] lineOutputs = getSegmentsFromLine(allSegments, "output");
+        for (String output : lineOutputs) {
+                outputs.add(sortString(output));
+        }
+        return outputs;
     }
 
-    public ArrayList<String> getAllInputs(ArrayList<String> rawInputArray){
-        ArrayList<String> inputs= new ArrayList<String>();
-        for (String line : rawInputArray){
+    public ArrayList<String> getAllInputs(ArrayList<String> rawInputArray) {
+        ArrayList<String> inputs = new ArrayList<String>();
+        for (String line : rawInputArray) {
             String[] allSegments = getAllSegmentsFromLine(line);
-            String[] lineInputs = getSegmentsFromLine(allSegments,"input");
-            for ( String input : lineInputs) {
-                inputs.add(input);
+            String[] lineInputs = getSegmentsFromLine(allSegments, "input");
+            for (String input : lineInputs) {
+                inputs.add(sortString(input));
             }
         }
-        return  inputs;
+        return inputs;
     }
 
-    public int decodeStringToDigit(String observedSegments) {
+    public ArrayList<String> getAllInputs(String rawInput) {
+        ArrayList<String> inputs = new ArrayList<String>();
+        String[] allSegments = getAllSegmentsFromLine(rawInput);
+        String[] lineInputs = getSegmentsFromLine(allSegments, "input");
+        for (String input : lineInputs) {
+            inputs.add(sortString(input));
+        }
+
+        return inputs;
+    }
+
+    public int decodeStringToDigitUniqueLength(String observedSegments) {
         int digit = -1;
-        if (observedSegments.length() == 2){
+        if (observedSegments.length() == 2) {
             digit = 1;
         }
-        if (observedSegments.length() == 3){
+        if (observedSegments.length() == 3) {
             digit = 7;
         }
-        if (observedSegments.length() == 4){
+        if (observedSegments.length() == 4) {
             digit = 4;
         }
-        if (observedSegments.length() == 7){
+        if (observedSegments.length() == 7) {
             digit = 8;
         }
         return digit;
@@ -91,13 +126,223 @@ public class Day8 extends Day {
 
     public int countDigitsWithUniqueNumberOfSegments(ArrayList<String> array) {
         int totalCount = 0;
-        for (String record: array){
-            int digit =  decodeStringToDigit(record);
-            if (digit != -1){
+        for (String record : array) {
+            int digit = decodeStringToDigitUniqueLength(record);
+            if (digit != -1) {
                 totalCount += 1;
             }
         }
         return totalCount;
 
     }
+
+    public HashMap<Integer, String> decodeUniqueLengthDigits(ArrayList<String> lineInputs) {
+        HashMap<Integer, String> segmentsEncodedDecoded = new HashMap<Integer, String>();
+        HashMap<String, String> encodingRules = new HashMap<String, String>();
+
+        for (String code : lineInputs) {
+            int decodedDigit = decodeStringToDigitUniqueLength(code);
+            if (decodedDigit != -1) {
+                segmentsEncodedDecoded.put(decodedDigit, code);
+            }
+
+        }
+        return segmentsEncodedDecoded;
+    }
+
+    public static String[] singleChars(String s) {
+        return s.split("(?!^)");
+    }
+
+    public  String[] intersection(String[] a, String[] b) {
+        return Stream.of(a)
+                .filter(Arrays.asList(b)::contains)
+                .toArray(String[]::new);
+    }
+
+    public String inAButNotInB(String[] a, String[] b) {
+        ArrayList<String> inAButNotInB = new ArrayList<>();
+        for (String aElement: a){
+            if (!Arrays.stream(b).anyMatch(aElement::equals) & !inAButNotInB.contains(aElement)){
+                inAButNotInB.add(aElement);
+            }
+        }
+        //return inAButNotInB.toArray(new String[0]);
+        return String.join("",inAButNotInB);
+    }
+
+    public String segmentsNotInEveryDigitsInArray(ArrayList<String> array){
+        String[] allSegments = "abcdefg".split("");
+        int numberOfDigits = array.size();
+        StringBuffer result = new StringBuffer();
+        for (String segment: allSegments){
+            int numberOfDigitsContainThisSegment = 0;
+            for (String digit : array){
+                if (digit.contains(segment)) {
+                    numberOfDigitsContainThisSegment += 1;
+                }
+            }
+            if( numberOfDigitsContainThisSegment != numberOfDigits){
+                result.append(segment);
+            }
+        }
+        return String.valueOf(result);
+    }
+
+    public String segmentsInEveryDigitsInArray(ArrayList<String> array){
+        String[] allSegments = "abcdefg".split("");
+        int numberOfDigits = array.size();
+        StringBuffer result = new StringBuffer();
+        for (String segment: allSegments){
+            int numberOfDigitsContainThisSegment = 0;
+            for (String digit : array){
+                if (digit.contains(segment)) {
+                    numberOfDigitsContainThisSegment += 1;
+                }
+            }
+            if( numberOfDigitsContainThisSegment == numberOfDigits){
+                result.append(segment);
+            }
+        }
+        return String.valueOf(result);
+    }
+
+    public String inArrayAButNotInArrayB( ArrayList<String> arrayA,
+                                              ArrayList<String> arrayB) {
+        String digitsAString = String.join("", arrayA);;
+        String[] aStringArray = digitsAString.split("");
+        String digitBString = String.join("", arrayB);;
+        String[] bStringArray = digitBString.split("");
+        String inAButNotInB = inAButNotInB(aStringArray, bStringArray);
+        return inAButNotInB;
+    }
+
+    public ArrayList<String> getAllEncodedDigitsOfLengthX(ArrayList<String> inputsArray, int X){
+        ArrayList<String> encodedDigits = new ArrayList<String>();
+        for (String input: inputsArray){
+            if( input.length()== X){
+                encodedDigits.add(input);
+            }
+        }
+        return encodedDigits;
+    }
+
+    public String removeDuplicates(StringBuffer sb){
+        return Arrays.asList(sb.toString().split(""))
+                .stream()
+                .distinct()
+                .collect(Collectors.joining());
+    }
+
+    public HashMap<Integer, String> decodeDigits(ArrayList<String> inputsArray){
+        HashMap<Integer, String> decodedDigits = decodeUniqueLengthDigits(inputsArray);
+        ArrayList<String> digitOne = new ArrayList<String>(Arrays.asList(decodedDigits.get(1)));
+        ArrayList<String> digitFour = new ArrayList<String>(Arrays.asList(decodedDigits.get(4)));
+        ArrayList<String> digitOneFourSeven = new ArrayList<String>(Arrays.asList(decodedDigits.get(1),
+                        decodedDigits.get(4),
+                        decodedDigits.get(7)));
+        ArrayList<String> digitSeven = new ArrayList<String>(Arrays.asList(decodedDigits.get(7)));
+        ArrayList<String> digitEight = new ArrayList<String>(Arrays.asList(decodedDigits.get(8)));
+        ArrayList<String> digitsOfLength5 = getAllEncodedDigitsOfLengthX(inputsArray, 5);
+        ArrayList<String> digitsOfLength6 = getAllEncodedDigitsOfLengthX(inputsArray, 6);
+
+        HashMap<String, String> encodingRules = new HashMap<String, String>();
+
+        /**Rule - In 1 but not in 7 **/
+        encodingRules.put("a",inArrayAButNotInArrayB(digitSeven,digitOne));
+
+
+        /**Rule - In all digits of length 6**/
+        encodingRules.put("abfg",segmentsInEveryDigitsInArray(digitsOfLength6));
+
+
+        /**Rule - Not in every digit of length 6**/
+        encodingRules.put("cde",segmentsNotInEveryDigitsInArray(digitsOfLength6));
+
+        /**Rule - In 8 but not in 1,4 or 7**/
+        encodingRules.put("eg",inArrayAButNotInArrayB(digitEight,digitOneFourSeven));
+
+        /**Rule - In all digits of length 5**/
+        encodingRules.put("adg",segmentsInEveryDigitsInArray(digitsOfLength5));
+
+        /**Rule - In 4 but not in 1**/
+        encodingRules.put("bd",inArrayAButNotInArrayB(digitFour,digitOne));
+
+        /**Digit 2**/
+        StringBuffer decodedDigitTwo = new StringBuffer();
+        decodedDigitTwo.append(encodingRules.get("a"));
+        decodedDigitTwo.append(encodingRules.get("cde"));
+        decodedDigitTwo.append(encodingRules.get("eg"));
+        String decodedTwo = sortString(removeDuplicates(decodedDigitTwo));
+        decodedDigits.put(2,decodedTwo);
+        encodingRules.put("acdeg",decodedTwo);
+
+        /**Digit 5**/
+        StringBuffer decodedDigitFive = new StringBuffer();
+        decodedDigitFive.append(encodingRules.get("abfg"));
+        decodedDigitFive.append(encodingRules.get("bd"));
+        String decodedFive = sortString(removeDuplicates(decodedDigitFive));
+        decodedDigits.put(5,decodedFive);
+        encodingRules.put("abdfg",decodedFive);
+
+        /**Digit 3**/
+        for (String key : digitsOfLength5) {
+            if (!key.equals(decodedFive) && !key.equals(decodedTwo)
+            )
+            {   String decodedThree = key;
+                decodedDigits.put(3,decodedThree);
+                encodingRules.put("acdfg",decodedThree);
+            }
+        }
+
+        /**Digit 6**/
+        StringBuffer decodedDigitSix = new StringBuffer();
+        decodedDigitSix.append(encodingRules.get("abdfg"));
+        decodedDigitSix.append(encodingRules.get("eg"));
+        String decodedSix = sortString(removeDuplicates(decodedDigitSix));
+        decodedDigits.put(6,decodedSix);
+        encodingRules.put("abdefg",decodedSix);
+
+        /**Rule - In 6 but not in 5**/
+        ArrayList<String> digitSix = new ArrayList<String>(Arrays.asList(decodedDigits.get(6)));
+        ArrayList<String> digitFive = new ArrayList<String>(Arrays.asList(decodedDigits.get(5)));
+        encodingRules.put("e",inArrayAButNotInArrayB(digitSix,digitFive));
+
+        /**Digit 0**/
+        String encodedLetterE = encodingRules.get("e");
+
+        for (String key : digitsOfLength6) {
+            if (key.contains(encodedLetterE) && !key.equals(decodedSix))
+            {   String decodedZero = key;
+                decodedDigits.put(0,decodedZero);
+                encodingRules.put("abcefg",decodedZero);
+            }
+        }
+
+        /**Digit 9**/
+        for (String key : digitsOfLength6) {
+            if (!key.equals(decodedDigits.get(0)) && !key.equals(decodedSix)
+            )
+            {   String decodedNine = key;
+                decodedDigits.put(9,decodedNine);
+                encodingRules.put("abcdfg",decodedNine);
+            }
+        }
+
+        return  decodedDigits;
+    }
+
+    public Integer decodeOutputDigits(ArrayList<String> outputsArray, HashMap<Integer, String> decodedInputs) {
+        StringBuffer outputString = new StringBuffer();
+        for (String outputDigit : outputsArray){
+            for (Integer digit : decodedInputs.keySet()) {
+                if ( outputDigit.equals(decodedInputs.get(digit))) {
+                    outputString.append(String.valueOf(digit));
+                }
+            }
+        }
+        return Integer.parseInt(outputString.toString());
+
+    }
+
 }
--- src/test/java/Day8Test.java
+++ src/test/java/Day8Test.java
@@ -3,6 +3,7 @@ import junit.framework.TestCase;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Day8Test extends TestCase {
     Day8 day8 = new Day8();
@@ -16,35 +17,16 @@ public class Day8Test extends TestCase {
 
     public void testPartTwoAnswer() throws FileNotFoundException {
         String actualAnswer = day8.partTwoAnswer(RESOURCE);
-        String expectedAnswer = "-1";
+        String expectedAnswer = "61229";
         assertEquals(expectedAnswer, actualAnswer);
     }
 
-    public void testGetAllSegments() {
-        String input = "be | fdgacbe";
-        String[]  expectedArray = {"be","|","fdgacbe"};
-        assertTrue(Arrays.deepEquals(expectedArray, day8.getAllSegmentsFromLine(input)));
-    }
-
-    public void testGetSegmentsInput() {
-        String[]  inputArray = {"be","cbdgef","|","fdgacbe"};
-        String[]  expectedArray = {"be","cbdgef"};
-        String[]  actualArray = day8.getSegmentsFromLine(inputArray, "input");
-        assertTrue(Arrays.deepEquals(expectedArray, actualArray));
-    }
-
-    public void testGetSegmentsOutput() {
-        String[]  inputArray = {"be","cbdgef","|","fdgacbe"};
-        String[]  expectedArray = {"fdgacbe"};
-        String[]  actualArray = day8.getSegmentsFromLine(inputArray, "output");
-        assertTrue(Arrays.deepEquals(expectedArray, actualArray));
-    }
 
     public void testGetAllOutputs() {
         ArrayList<String> inputArrayString = new ArrayList<String>(
                 Arrays.asList("be | fdgacbe", "fgdeca fcdbega | efabcd yyy"));
         ArrayList<String> expectedArray = new ArrayList<String>(
-                Arrays.asList("fdgacbe", "efabcd", "yyy"));
+                Arrays.asList("abcdefg", "abcdef", "yyy"));
         ArrayList<String> actualArray = day8.getAllOutputs(inputArrayString);
         assertEquals(actualArray.get(0), expectedArray.get(0));
         assertEquals(actualArray.get(1), expectedArray.get(1));
@@ -55,7 +37,7 @@ public class Day8Test extends TestCase {
         ArrayList<String> inputArrayString = new ArrayList<String>(
                 Arrays.asList("be | fdgacbe", "fgdeca fcdbega | efabcd yyy"));
         ArrayList<String> expectedArray = new ArrayList<String>(
-                Arrays.asList("be", "fgdeca", "fcdbega"));
+                Arrays.asList("be", "acdefg", "abcdefg"));
         ArrayList<String> actualArray = day8.getAllInputs(inputArrayString);
         assertEquals(actualArray.get(0), expectedArray.get(0));
         assertEquals(actualArray.get(1), expectedArray.get(1));
@@ -65,35 +47,35 @@ public class Day8Test extends TestCase {
     public void testDecodeStringToDigitTwo() {
         String input = "ab";
         int expectedDigit = 1;
-        int actualDigit = day8.decodeStringToDigit(input);
+        int actualDigit = day8.decodeStringToDigitUniqueLength(input);
         assertEquals(actualDigit, expectedDigit);
     }
 
     public void testDecodeStringToDigitThree() {
         String input = "abb";
         int expectedDigit = 7;
-        int actualDigit = day8.decodeStringToDigit(input);
+        int actualDigit = day8.decodeStringToDigitUniqueLength(input);
         assertEquals(actualDigit, expectedDigit);
     }
 
     public void testDecodeStringToDigitFour() {
         String input = "abbc";
         int expectedDigit = 4;
-        int actualDigit = day8.decodeStringToDigit(input);
+        int actualDigit = day8.decodeStringToDigitUniqueLength(input);
         assertEquals(actualDigit, expectedDigit);
     }
 
     public void testDecodeStringToDigitSeven() {
         String input = "abbcabc";
         int expectedDigit = 8;
-        int actualDigit = day8.decodeStringToDigit(input);
+        int actualDigit = day8.decodeStringToDigitUniqueLength(input);
         assertEquals(actualDigit, expectedDigit);
     }
 
     public void testDecodeStringToDigitOther() {
         String input = "abbabcabcdb";
         int expectedDigit = -1;
-        int actualDigit = day8.decodeStringToDigit(input);
+        int actualDigit = day8.decodeStringToDigitUniqueLength(input);
         assertEquals(actualDigit, expectedDigit);
     }
 
@@ -105,6 +87,77 @@ public class Day8Test extends TestCase {
         assertEquals(actualCount, expectedCount);
     }
 
+    public void testDecodeAllDigits(){
+        ArrayList<String> inputArrayString = new ArrayList<String>(
+                Arrays.asList("acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"));
+        day8.decodeUniqueLengthDigits(inputArrayString);
+    }
+
+    public void testInArrayAButNotInArrayB(){
+        ArrayList<String> inputArrayA = new ArrayList<String>(
+                Arrays.asList("abcd", "efdsr", "wwey"));
+        ArrayList<String> inputArrayB = new ArrayList<String>(
+                Arrays.asList("abcd", "efdsr", "zzz"));
+        String actualArray = day8.inArrayAButNotInArrayB(inputArrayA,inputArrayB);
+        String expectedArray = "wy";
+        assertEquals(expectedArray,actualArray);
+    }
+
+    public void testIntersection(){
+        String[] a = {"a","b"};
+        String[] b = {"b","c", "d"};
+        String[] actualArray = day8.intersection(a,b);
+        String[] expectedArray = {"b"};
+        assertTrue(Arrays.deepEquals(expectedArray, actualArray));
+    }
+
+    public void testInAButNotInB(){
+        String[] a = {"a","b"};
+        String[] b = {"b","c", "d"};
+        String actualArray = day8.inAButNotInB(a,b);
+        String expectedArray = "a";
+        assertEquals(expectedArray,actualArray);
+    }
+
+    public void testSingleChars() {
+        String inputString = "abc";
+        String[] actualArray = day8.singleChars(inputString);
+        String[] expectedArray = {"a","b","c"};
+        assertTrue(Arrays.deepEquals(expectedArray, actualArray));
+    }
+
+    public void testDecodeDigits(){
+        ArrayList<String> inputsArray = new ArrayList<String>(
+                Arrays.asList("acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"));
+        HashMap<Integer, String> actualMap = day8.decodeDigits(inputsArray);
+        HashMap<Integer, String> expectedMap = new HashMap<>();
+        expectedMap.put(0,"cagedb");
+        expectedMap.put(1,"ab");
+        expectedMap.put(2,"acdfg");
+        expectedMap.put(3,"fbcad");
+        expectedMap.put(4,"eafb");
+        expectedMap.put(5,"bcdef");
+        expectedMap.put(6,"bcdefg");
+        expectedMap.put(7,"dab");
+        expectedMap.put(8,"acedgfb");
+        expectedMap.put(9,"cdfgeb");
+        assertEquals(expectedMap,actualMap);
+    }
+
+    public void testError(){
+        String l = "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb";
+        ArrayList<String> outputsArray = day8.getAllOutputs(l);
+        ArrayList<String> inputsArray = day8.getAllInputs(l);
+        System.out.println("INPUTS");
+        for (String in : inputsArray){
+            System.out.println(in);
+        }
+        System.out.println("INPUTS");
+        HashMap<Integer, String> decodeDigitsMap = day8.decodeDigits(inputsArray);
+        Integer decodeOutputDigits = day8.decodeOutputDigits(outputsArray, decodeDigitsMap);
+        System.out.println("record " + l + " output " + decodeOutputDigits);
+
+    }
 
 
 }
