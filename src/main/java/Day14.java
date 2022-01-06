a1e663104d3c1083758664b01ee19cf23934de67
Natalia Kukushkina <natkk@amazon.com>
2022-01-05 20:46:52 -0500

    day 14 part 1 solution

---
+++ src/main/java/Day14.java
@@ -0,0 +1,107 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Day14 extends Day  {
+    static String RESOURCE = "src/main/resources/day14_input.txt";
+
+    public Day14() {
+        super();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //System.out.println("Part1: " + new Day14().partOneAnswer(RESOURCE));
+        System.out.println("Part2: " + new Day14().partTwoAnswer(RESOURCE));
+    }
+
+    @Override
+    public String partOneAnswer(String resource) throws FileNotFoundException {
+        ArrayList<String> inputArray = getResourceAsStringArray(resource);
+        HashMap<String,String> instructions = getInstructionsHashMap(inputArray);
+        int numberOfSteps = 10;
+        String polymer = inputArray.get(0);
+        for (int i = 0; i < numberOfSteps; i++){
+            polymer = processStep(polymer, instructions);
+        }
+        HashMap<Character,Long> frequencies = getFrequencyOfElements(polymer);
+        long answer = differenceBetweenFrequencies(frequencies);
+        return String.valueOf(answer);
+    }
+
+    @Override
+    public String partTwoAnswer(String resource) {
+        ArrayList<String> inputArray = getResourceAsStringArray(resource);
+        HashMap<String,String> instructions = getInstructionsHashMap(inputArray);
+        int numberOfSteps = 30;
+        String polymer = inputArray.get(0);
+        for (int i = 0; i < numberOfSteps; i++){
+            System.out.println("step "+ i);
+            polymer = processStep(polymer, instructions);
+
+        }
+        HashMap<Character,Long> frequencies = getFrequencyOfElements(polymer);
+        long answer = differenceBetweenFrequencies(frequencies);
+        return String.valueOf(answer);
+    }
+
+    public String processStep(String polymer, HashMap<String,String> instructions){
+        StringBuffer stringBuffer  = new StringBuffer();
+        for (int i = 0; i < polymer.length() - 1; i++){
+            String firstLetter = polymer.substring(i, i+1);
+            String pair = polymer.substring(i, i+2);
+            String toInsert = instructions.get(pair);
+            stringBuffer.append(firstLetter);
+            stringBuffer.append(toInsert);
+
+        }
+        String lastLetter = polymer.substring(polymer.length() -1, polymer.length());
+        stringBuffer.append(lastLetter);
+        return stringBuffer.toString();
+    }
+
+    public long differenceBetweenFrequencies(HashMap<Character,Long> frequencies){
+        long minFreq = 0;
+        long maxFreq = 0;
+        for (Character key : frequencies.keySet()) {
+            long value = frequencies.get(key);
+            if (minFreq == 0 && maxFreq == 0){
+                minFreq = value;
+                maxFreq = value;
+            }
+            if (value > maxFreq) {
+                maxFreq = value;
+            }
+            if (value < minFreq){
+                minFreq = value;
+            }
+        }
+        return maxFreq - minFreq;
+    }
+
+    public HashMap<Character,Long> getFrequencyOfElements(String polymer) {
+        HashMap<Character,Long> frequencies = new HashMap<Character, Long>();
+        for (int i = 0; i < polymer.length(); i++) {
+            char c = polymer.charAt(i);
+            Long freq = frequencies.get(c);
+            if (freq != null) {
+                frequencies.put(c, new Long(freq + 1));
+            }
+            else {
+                frequencies.put(c, Long.valueOf(1));
+            }
+        }
+        return frequencies;
+    }
+
+    public HashMap<String,String> getInstructionsHashMap(ArrayList<String> inputArray) {
+        HashMap <String,String> instructionsHashMap = new HashMap<>();
+        for (String line : inputArray){
+            if (line.contains("->")) {
+                String pair = line.substring(0, 2);
+                String replacement = line.substring(6);
+                instructionsHashMap.put(pair,replacement);
+            }
+        }
+        return instructionsHashMap;
+    }
+}
---
+++ src/test/java/Day14Test.java
@@ -0,0 +1,38 @@
+import junit.framework.TestCase;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Day14Test extends TestCase {
+    Day14 day14 = new Day14();
+    private static final String RESOURCE = "src/test/resources/day14_test.txt";
+
+    public void testPartOneAnswer() throws FileNotFoundException {
+        String actualAnswer = day14.partOneAnswer(RESOURCE);
+        String expectedAnswer = "1588";
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    public void testLoopOverPairs(){
+        HashMap <String,String> instructions = new HashMap<>();
+        instructions.put("NN","C");
+        instructions.put("NC","B");
+        instructions.put("CB","H");
+        String polymer = "NNCB";
+        String expected = "NCNBCHB";
+        String actual = day14.processStep(polymer,instructions);
+        assertEquals(expected,actual);
+    }
+
+    public void testGetInstructionsHashMap() {
+        ArrayList<String> inputArrayString = new ArrayList<String>(
+                Arrays.asList("KO -> H", "CH -> B"));
+        HashMap <String,String> actualMap = day14.getInstructionsHashMap(inputArrayString);
+        HashMap <String,String> expectedMap = new HashMap<>();
+        expectedMap.put("KO","H");
+        expectedMap.put("CH","B");
+        assertEquals(expectedMap,actualMap);
+    }
+
+}
